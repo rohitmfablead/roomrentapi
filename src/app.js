1c09caf4acf5e0d9d03b9fdbb07a4bb47c118944
rohitmfablead <rohit.fablead@gmail.com>
2025-12-11 17:02:52 +0530

    pakeges problem fix this

--- src/app.js
+++ src/app.js
@@ -22,6 +22,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 // routes
 app.use("/api/auth", authRoutes);
 app.use("/api/rooms", roomRoutes);
